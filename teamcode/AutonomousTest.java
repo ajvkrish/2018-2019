1fecdd71e747b350a59004e199398d520dbe4eba
5119baryons <44823744+5119baryons@users.noreply.github.com>
2018-12-08 10:53:19 -0600

    Add files via upload

--- teamcode/AutonomousTest.java
+++ teamcode/AutonomousTest.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AutonomousTest {
+import org.firstinspires.ftc.teamcode.TwoControllerTeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+
+
+@Autonomous(name = "TTest Auto")
+public class AutonomousTest extends LinearOpMode {
+    private Robot robot;
+    private TwoControllerTeleOp teleop;
+    @Override
+    public void runOpMode() {
+        robot = new Robot(hardwareMap, telemetry);
+        teleop=new TwoControllerTeleOp();
+        telemetry.addData("Initialized","");
+        telemetry.update();
+        waitForStart();
+        teleop.init();
+        telemetry.addData("second init","");
+        telemetry.update();
+        teleop.loop();
+        telemetry.addData("Finished","");
+        telemetry.update();
+    }
 }
